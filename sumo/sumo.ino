8385b3d3e55bcc67786d7d2dc15bb71f170c7d3a
PMTaylor <bartmusicians@gmail.com>
2017-04-16 19:01:28 -0700

    verbose variable names, timing, multiple graphs
    Update creates more consistent variable names, added depth to comments,
    formatted serial output to read well in serial monitor, and graph in the
    serial plotter.

--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -16,77 +16,98 @@
    A1: Current sense of Motor B 
    A4: SDA (BLUE)
    A5: SCL (GREEN)
+
+   Sensors use 5v pin for power, GND for ground.
  */
-#define PWMA 5
-#define PWMB 6
-#define leftQtr 2
-#define rightQtr 3
-#define leftx 2
-#define rightx 3
+//================= temp /debugging variables   ========================
+long time_since_last_read;
 
-#define leftAddr  22
-#define rightAddr  23
 
-#define NUM_SENSORS             1 // number of sensors used
-#define NUM_SAMPLES_PER_SENSOR  2  // average 4 analog samples per sensor reading
-#define EMITTER_PIN             QTR_NO_EMITTER_PIN  // emitter is controlled by digital pin 2
+//================= robot control variables   ========================
+#define SPEED 0                            //max motor speed. range is 0-255. 255 = 100% duty PWM, which is battery input voltage (14.8v for 4cell lipo, 11.1v for 3cell lipo)
+#define TOF_MAX_RANGE 400                   // throw away ranges longer than this, to avoid noise and possibly sensing people too close to the ring. 
 
-#define NUM_READINGS 10
+
+//================= hardware variables        ========================
+#define pwm_left_motor          5           //power to left motor pin
+#define pwm_right_motor         6           //power to right motor pin
+#define dir_left_motor          4           //direction to left motor pin
+#define dir_right_motor         7           //direction to right motor pin
+
+#define left_qtr                2           //left edge sensor pin
+#define right_qtr               3           //right edge sensor pin
+#define left_x                  2           //left TOF sensor "xshut" shutdown logic pin
+#define right_x                 3           //right TOF sensor "xshut" shutdown logic pin
+
+#define left_addr               22          //left TOF sensor I2C address
+#define right_addr              23          //right TOF sensor I2C address
+
+#define NUM_QTR_SENSORS               2     // number of sensors used
+#define NUM_SAMPLES_PER_QTR_SENSOR    4     // average 4 analog samples per sensor reading
+#define EMITTER_PIN                   QTR_NO_EMITTER_PIN  // emitter is controlled by digital pin 2
+
+#define NUM_READINGS                  10
+
+#define TIMING_BUDGET_US              20000  //minimum internally limited to 20000, pushing this below 20000 returns it to the default of 33ms :-(
+
+//what are these for?
 int leftReadings[NUM_READINGS];
 int rightReadings[NUM_READINGS];
 int leftHistory[NUM_READINGS];
 int rightHistory[NUM_READINGS];
 
 // sensors 0 through 1 are connected to analog inputs 0 through 1, respectively
-QTRSensorsAnalog qtra((unsigned char[]) {leftQtr}, NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
-unsigned int sensorValues[NUM_SENSORS];
+QTRSensorsAnalog qtra((unsigned char[]) {left_qtr, right_qtr}, NUM_QTR_SENSORS, NUM_SAMPLES_PER_QTR_SENSOR, EMITTER_PIN);
+unsigned int sensorValues[NUM_QTR_SENSORS]; // 2 qtr sensors, 2 sent to array constructor, but array has indices [0] and [1] only
 
-
-
-VL53L0X left_tSensor;
-VL53L0X right_tSensor;
+VL53L0X left_tSensor;                       //left TOF sensor object
+VL53L0X right_tSensor;                      //right TOF sensor object
 
 
 void setup()
-{
-  pinMode(PWMA, OUTPUT);
-  pinMode(PWMB, OUTPUT);
-  pinMode(leftx, OUTPUT); //Pin of right sensor XSHUT
-  pinMode(rightx, OUTPUT); //Pin of left sensor XSHUT
-  digitalWrite(leftx, LOW); //Drive them low to turn them off..
-  digitalWrite(rightx, LOW); //
+{ 
+  //"Pins configured as OUTPUT with pinMode() are said to be in a low-impedance state. 
+  //This means that they can provide a substantial amount of current (40ma) to other circuits. "
+  pinMode(pwm_left_motor, OUTPUT);          
+  pinMode(pwm_right_motor, OUTPUT);
+  pinMode(left_x, OUTPUT); //Pin of right sensor XSHUT
+  pinMode(right_x, OUTPUT); //Pin of left sensor XSHUT
+
+  //shut down the two TOF sensors so we can choose their addresses one at a time
+  digitalWrite(left_x, LOW); //Drive them low to turn them off..
+  digitalWrite(right_x, LOW); //
   delay(500); //Chill for a sec
   Wire.begin(); // Start i2c listening...
   
   Serial.begin (250000);
 
-  pinMode(leftx, INPUT); //Turn left sensor on
+  //Turn left sensor on, address it on I2C
+  pinMode(left_x, INPUT);
+  digitalWrite(left_x, HIGH); //Drive high to turn on..
   delay(150);
   Serial.println("00");
   left_tSensor.init(true);
-
   Serial.println("01");
   delay(100);
-  left_tSensor.setAddress((uint8_t)leftAddr);
+  left_tSensor.setAddress((uint8_t)left_addr);
   Serial.println("02");
 
-  pinMode(rightx, INPUT);
+  //Turn right sensor on, address it on I2C
+  pinMode(right_x, INPUT);
+  digitalWrite(right_x, HIGH); //Drive high to turn on..
   delay(150);
   right_tSensor.init(true);
   Serial.println("03");
   delay(100);
-  right_tSensor.setAddress((uint8_t)rightAddr);
+  right_tSensor.setAddress((uint8_t)right_addr);
   Serial.println("04");
   Serial.println("addresses set");
 
-
+  //print addresses of found sensors
   Serial.println ("I2C scanner. Scanning ...");
   byte count = 0;
-
-
   for (byte i = 1; i < 120; i++)
   {
-
     Wire.beginTransmission (i);
     if (Wire.endTransmission () == 0)
     {
@@ -105,51 +126,67 @@ void setup()
   Serial.print (count, DEC);
   Serial.println (" device(s).");
   delay(50);
-  left_tSensor.setMeasurementTimingBudget(20000);
+
+  // set timing mode for sensors, allowing a short budget in microseconds. 20,000us = 20ms = 50Hz read rate. 20ms is apparently the minimum that works. 
+  //wondering if we should try something smaller to increase the refresh rate, since we don't care at all about range, just presence of an object.
+  left_tSensor.setMeasurementTimingBudget(TIMING_BUDGET_US);
   left_tSensor.startContinuous();
-  right_tSensor.setMeasurementTimingBudget(20000);
+  right_tSensor.setMeasurementTimingBudget(TIMING_BUDGET_US);
   right_tSensor.startContinuous();
   delay(500);
 }
-#define SPEED 70
+
 void loop()
 {
- //qtra.read(sensorValues);
- //Serial.print("Analog Values: ");
- //Serial.println(sensorValues[0]);
- //delay(100);
- //Serial.print("Time of flight distance (Left): ");
- int leftRead = left_tSensor.readRangeContinuousMillimeters(); 
- if(leftRead < 400)
- {
-   analogWrite(PWMA, SPEED);
-   Serial.print(leftRead);
- }
- else
- {
-   analogWrite(PWMA, 0);
-   Serial.print(550);
- }
- Serial.print(" ");
- //if (left_tSensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
- //Serial.println();
- //Serial.print("Time of flight distance (Right): ");
- int rightRead = right_tSensor.readRangeContinuousMillimeters(); 
- if(rightRead < 400)
- {
-   analogWrite(PWMB,SPEED);
-   Serial.print(rightRead);
- }
- else
- {
-   analogWrite(PWMB,0);
-   Serial.print(550);
- }
- //if (right_tSensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
- Serial.println();
- //delay(10);
+  //timing test
+  time_since_last_read = millis()-time_since_last_read;
+  Serial.print("ms since last read : ");
+  Serial.print(time_since_last_read);
+  Serial.print(" , ");
+  time_since_last_read = millis();
+
+  //edge sensor reading and serial output
+  qtra.read(sensorValues);
+  Serial.print("QTR Analog Values: ");
+  Serial.print(sensorValues[0]);
+  Serial.print(" , ");
+
+  //left sensor
+  Serial.print("Time of flight distance (Left): ");
+  int leftRead = left_tSensor.readRangeContinuousMillimeters();
+  if(leftRead < TOF_MAX_RANGE)
+  {
+    analogWrite(pwm_left_motor, SPEED);
+    Serial.print(leftRead);
+  }
+  else
+  {
+    analogWrite(pwm_left_motor, 0);
+    Serial.print(550);
+  }
+  Serial.print(" , ");
+
+  //right sensor
+  Serial.print("Time of flight distance (Right): ");
+  int rightRead = right_tSensor.readRangeContinuousMillimeters(); 
+  if(rightRead < TOF_MAX_RANGE)
+  {
+    analogWrite(pwm_right_motor,SPEED);
+    Serial.print(rightRead);
+  }
+  else
+  {
+    analogWrite(pwm_right_motor,0);
+    Serial.print(550);
+  }
+  Serial.print(" , ");
+  Serial.println();
+
 }
 
+
+/*
+ * unused function? no yet useful?
 void readValues(VL53L0X* sensor, uint32_t* dest, size_t numReadings)
 {
   for(int i = 0; i < numReadings; i++)
@@ -157,4 +194,4 @@ void readValues(VL53L0X* sensor, uint32_t* dest, size_t numReadings)
     *(dest + i ) = sensor->readRangeSingleMillimeters();
   }
 }
-
+*/
